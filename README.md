be44c526c0208a70657ab32bdd45f9d89e2ef8c7
wangliang4 <wangliang4@newhope.cn>
2024-06-14 16:52:13 +0800

    feat(README): 完善README

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+## 功能概述
+该组件是对Element Plus的<el-select>组件的封装，增强了筛选功能，实现了拼音辅助的模糊搜索匹配。
+
+**特性**
+* 模糊搜索：通过filterable属性启用el-select的筛选功能，并自定义filter-method来实现基于拼音的匹配逻辑。
+* 拼音转换：使用pinyin库将选项标签转换为拼音，以辅助搜索, 例如：options中有一个`label`为`"深圳"`，则可以通过`sz`来进行搜索。
+* 高亮匹配：在搜索结果中匹配到的部分会以<span class="highlight">...</span>包裹，用于样式高亮显示, 支持非连续字符串的高亮。
+
 ## 使用方式
 
 **安装依赖**
@@ -26,3 +34,6 @@ app.use(ElProSelect)
 app.mount('#app')
 
 ```
+
+**示例** 
+![1](https://github.com/JOU-amjs/sdm2/assets/29848971/f7f5fd21-d49b-45b6-94d4-e94c25326f0c)
--- example/src/components/HelloWorld.vue
+++ example/src/components/HelloWorld.vue
@@ -10,41 +10,36 @@ const count = ref(0)
 const value = ref('')
 
 const options = [
-  {
-    label: '选项1',
-    value: 'shanghai',
+{
+    value: 'Beijing',
+    label: '北京',
   },
   {
-    label: '选项2',
-    value: 'beijing',
+    value: 'Shanghai',
+    label: '上海',
+  },
+  {
+    value: 'Nanjing',
+    label: '南京',
+  },
+  {
+    value: 'Chengdu',
+    label: '成都',
+  },
+  {
+    value: 'Shenzhen',
+    label: '深圳',
+  },
+  {
+    value: 'Guangzhou',
+    label: '广州',
   },
 ]
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <el-pro-select v-model="value" :options="options"></el-pro-select>
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <el-pro-select v-model="value" :options="options"></el-pro-select>
+ 
 </template>
 
 <style scoped>
